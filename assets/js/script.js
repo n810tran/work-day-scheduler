b43fa267c50ffed8484916cf073315ebaeca142d
n810tran <nathaniel.k.tran@gmail.com>
2023-08-31 01:45:13 -0700

    fixed ids for time slots on html, began check time function on js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,13 +9,39 @@ $(function () {
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
     //
-    $('.saveBtn').on('click',)
+    $('.saveBtn').on('click', function saveText() {
+
+    });
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
+    let test = parseInt($('#hour-13').attr('id').split('hour-')[1]);
+    let currentTime = dayjs().format('H');
+    console.log(test, currentTime);
+    if (currentTime < test) {console.log('past')}
+
+  function checkTime() {
+    let currentTime = dayjs().format('H');
+    let timeSlot = parseInt($('.time-block').attr('id').split('hour-')[1]);
+
+    if (timeSlot < currentTime) {
+      $('.time-block').addClass('past');
+      $('.time-block').removeClass('present');
+      $('.time-block').removeClass('future');
+    }
+    else if (timeSlot == currentTime) {
+      $('.time-block').removeClass('past');
+      $('.time-block').addClass('present');
+      $('.time-block').removeClass('future');
+    }
+    else {
+      $('.time-block').removeClass('past');
+      $('.time-block').removeClass('present');
+      $('.time-block').addClass('future');
+    }
+  }
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
